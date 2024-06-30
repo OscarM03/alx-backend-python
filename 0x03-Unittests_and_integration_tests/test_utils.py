4d655bba21cbeb889bcfbecb78a61e08d99088f2
OscarM03 <oscarmutwiri03@gmail.com>
2024-06-30 19:54:41 +0300

    Using memoization for testing

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ import unittest
 import requests
 from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -49,6 +49,33 @@ class TestGetJson(unittest.TestCase):
         results = get_json(url)
         mock_get.assert_called_once_with(url)
         self.assertEqual(results, payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Memoization"""
+    def test_memoize(self):
+        """test method"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method') as mock_method:
+            mock_method.return_value = 42
+
+            instance = TestClass()
+
+            res1 = instance.a_property
+            res2 = instance.a_property
+
+            mock_method.assert_called_once()
+
+            self.assertEqual(res1, 42)
+            self.assertEqual(res2, 42)
 
 
 if __name__ == "__main__":
